af342cd1f610e957b370c16579f5af95795227fc
littlebaiyi <1762417688@qq.com>
2020-02-25 20:23:37 +0800

    1

--- src/views/course/question/component/questionTypeNav.vue
+++ src/views/course/question/component/questionTypeNav.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 子菜单 -->
   <div class="my_tabs">
-    <div class="center font14 hgt40 my_tab_bars relative">
+    <div class="flex_mid font14 hgt40 my_tab_bars relative">
       <router-link
         exact
         :to="{name: item.name,query:{Id:id}}"
